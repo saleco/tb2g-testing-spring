0e71b901b7da1590471d125b9d6bdd9047272cdd
Sallo Szrajbman <salloszrajbman@hotmail.com>
2021-03-15 21:25:26 +0000

    Yanny or Laurel Spring Context with jUnit4 / jUnit 5 tests

---
+++ src/test/java/org/springframework/samples/petclinic/sfg/LaurelConfig.java
@@ -0,0 +1,15 @@
+package org.springframework.samples.petclinic.sfg;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.samples.petclinic.sfg.LaurelWordProducer;
+
+@Configuration
+public class LaurelConfig {
+
+    @Bean
+    LaurelWordProducer laurelWordProducer() {
+        return new LaurelWordProducer();
+    }
+}
